57ef6d5736bb4041308fd2f60c7466eecb1f4d14
Avantika <avantika.kala@gmail.com>
2022-02-08 20:30:37 +0530

    Added variables for file paths

--- src/clinicalDecisionSupportSystem/ClinicalDecisionSupportSystem.java
+++ src/clinicalDecisionSupportSystem/ClinicalDecisionSupportSystem.java
@@ -6,11 +6,14 @@ import ml.approach.Replace;
 
 public class ClinicalDecisionSupportSystem {
 
+	static final String TRAIN1_FILE_PATH = "E:\\dataset\\Allergy_Data1_ubtrain.csv";
+	static final String TRAIN2_FILE_PATH = "E:\\dataset\\Allergy_Data2_ubtrain.csv";
+
 	public static void main(String args[]) {
 
 		ClinicalDecisionObject cdss = MethodFactory.getMethod(CDSSConstants.METHOD_REPLACE);
 
-		cdss.initialize("E:\\dataset\\Allergy_Data1_ubtrain.csv", "E:\\dataset\\Allergy_Data2_ubtrain.csv", null, null);
+		cdss.initialize(TRAIN1_FILE_PATH, TRAIN2_FILE_PATH, null, null);
 		try {
 			cdss.performTest(CDSSConstants.ML_ALGORITHM_K_NEAREST_NEIGHBOUR);
 		} catch (Exception e) {
